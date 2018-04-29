267fd68bfed38281226ccbcad0548ecfc5f00aff
jpweiser <jon@jpweiser.com>
2018-04-29 16:51:15 -0500

    basic app layout with semantic-ui compoenents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import Write from './components/Write/Write';
-import About from './components/about/About';
-import {Menu, Segment} from 'semantic-ui-react'
+import {About, Write, LogoBox, RandomObject} from './components';
+import {Menu, Container, Visibility} from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      activeItem: 'About'
+      activeItem: 'About',
+      menuFixed: false
     }
   }
 
@@ -18,25 +18,35 @@ class App extends Component {
 
   content = () => {
     let _dict = {
-      About: <About />,
-      Write: <Write />
+      About: <About/>,
+      Write: <Write/>,
+      'Random Object': <RandomObject/>
     }
     return _dict[this.state.activeItem]
   }
+
+  stickTopMenu = () => this.setState({menuFixed: true})
+
+  unStickTopMenu = () => this.setState({menuFixed: false})
+
   render() {
     const {activeItem} = this.state;
     return (<div className="App">
-        <h1>Object Writing</h1>
-        <Segment>
-          <Menu pointing>
-            <Menu.Item name='About' active={activeItem === 'About'} onClick={this.handleItemClick}/>
-            <Menu.Item name='Write' active={activeItem === 'Write'} onClick={this.handleItemClick}/>
-          </Menu>
 
-          <div>{this.content()}</div>
-        </Segment>
+      <LogoBox/>
 
-      </div>);
+      <Visibility onTopPassed={this.stickTopMenu} onTopVisible={this.unStickTopMenu} once={false}>
+        <Menu inverted borderless fixed={this.state.menuFixed && 'top'} pointing="pointing">
+        {this.state.menuFixed && <Menu.Item className='lilLogo' header name='Object Writing:' />}
+        <Menu.Item name='About' active={activeItem === 'About'} onClick={this.handleItemClick}/>
+        <Menu.Item name='Write' active={activeItem === 'Write'} onClick={this.handleItemClick}/>
+        <Menu.Item name='Random Object' active={activeItem === 'Random Object'} onClick={this.handleItemClick}/>
+      </Menu>
+      </Visibility>
+      <Container className='AppContent'>
+        <div>{this.content()}</div>
+      </Container>
+    </div>);
   }
 }
 
